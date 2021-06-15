ad7afa874d07163f37b9d3a21e80653fc207efc9
Dwitrisha <59766408+Dwitrisha@users.noreply.github.com>
2021-06-15 18:44:54 +0530

    Added chat with dark mode

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './Components/Header';
+import Call from './Components/Pages/Call'
+import Calendar from './Components/Pages/Calendar'
+import Chat from './Components/Pages/Chat'
+import Sidebar from './Components/Sidebar'
+function App() {
+  return (
+    <div className="App">
+       <Header/>
+       <Chat/>
+    
+    </div>
+  );
+}
+
+export default App;
